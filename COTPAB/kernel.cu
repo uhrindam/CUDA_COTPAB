f17288f84618185b386099a35593b34753fe05d8
Uhrin Ádám <adamzek@outlook.hu>
2018-03-13 00:54:52 +0100

    slic

--- COTPAB/kernel.cu
+++ COTPAB/kernel.cu
@@ -4,7 +4,7 @@
 
 #include <stdio.h>
 #include <opencv2\opencv.hpp>
-
+#include "slic.h"
 
 using namespace std;
 using namespace cv;
@@ -12,8 +12,34 @@ using namespace cv;
 
 int main()
 {
-	Mat image = imread("C:\\Users\\Adam\\Desktop\\samples\\hulk2.jpg", CV_LOAD_IMAGE_COLOR);
+	/* Load the image and convert to Lab colour space. */
+	Mat image = imread("C:\\Users\\Adam\\Desktop\\samples\\completed.jpg", 1);
+	Mat lab_image = image.clone();
+	cvtColor(image, lab_image, CV_BGR2Lab);
 
-	imshow("Display window", image);
-	waitKey();
+	/* Yield the number of superpixels and weight-factors from the user. */
+	int w = image.cols;
+	int h = image.rows;
+	int nr_superpixels = 5000;
+	int nc = 80;
+
+	double step = (sqrt((w * h) / (double)nr_superpixels));
+	//1400*900-as képnél, 1000 superpixellel --> 35,496 --> vízszintesen 39,444, függõlegesen 25,354
+
+	/* Perform the SLIC superpixel algorithm. */
+	Slic slic;
+	slic.generate_superpixels(lab_image, step, nc);
+	slic.create_connectivity(lab_image);
+
+	/* Display the contours and show the result. */
+	Mat tt = image.clone();
+	slic.display_contours(tt, Vec3b(0, 0, 255));
+	imwrite("C:\\Users\\Adam\\Desktop\\0MATsamplewitchLines.jpg", tt);
+
+	//----------------------
+	slic.colour_with_cluster_means(image);
+	imwrite("C:\\Users\\Adam\\Desktop\\1MATsamplefilled.jpg", image);
+	//----------------------
+
+	cvWaitKey(0)
 }
